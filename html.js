0f709fb8446f256250dd6865676353ebe2f1ec41
saqlain shaikh <118292650+Saq65@users.noreply.github.com>
2023-04-08 15:32:44 +0530

    Update html.js

--- html.js
+++ html.js
@@ -21,7 +21,7 @@ function Click(){
     }
 else{
 document.getElementById('f1').innerHTML='valid';
-document.getElementById('a1').style.borderColor='green';
+document.getElementById('fname').style.borderColor='green';
 }
     var a2=document.getElementById('lname').value;
     if(a2==""){
