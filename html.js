449cefa9bdbb4bb74e0261ceddf6e5e7e193ffbc
saqlain shaikh <118292650+Saq65@users.noreply.github.com>
2023-04-08 12:35:14 +0530

    Add files via upload

---
+++ html.js
@@ -0,0 +1,61 @@
+function Click(){
+    var a1=document.getElementById('fname').value;
+    if(a1==""){
+        document.getElementById('f1').innerHTML='Please fill this input';
+        return false;
+    }
+    else if(a1.length <3)
+    {
+        document.getElementById('f1').innerHTML='Minimum 3 letter name write';
+        return false;
+    }
+    else if(a1.length >20)
+    {
+        document.getElementById('f1').innerHTML='max 20 letter name allowed';
+        return false;
+    }
+    else if(!isNaN(a1))
+    {
+        document.getElementById('f1').innerHTML='only alphabet allowed';
+        return false;
+    }
+    var a2=document.getElementById('lname').value;
+    if(a2==""){
+        document.getElementById('f2').innerHTML='Please fill this input';
+        return false;
+    } else if(a2.length <3)
+    {
+        document.getElementById('f2').innerHTML='Minimum 3 letter name write';
+        return false;
+    }
+    else if(a2.length >25)
+    {
+        document.getElementById('f2').innerHTML='max 25 letter name allowed';
+        return false;
+    }
+    else if(!isNaN(a2))
+    {
+        document.getElementById('f2').innerHTML='only alphabet allowed';
+        return false;
+    }
+    var a3=document.getElementById('uname').value;
+    if(a3==""){
+        document.getElementById('f3').innerHTML='Please fill username';
+        return false;
+    }
+    var a4=document.getElementById('p').value;
+    if(a4==""){
+        document.getElementById('f4').innerHTML='Please create passward';
+        return false;
+    }
+    if(a4!==a5){
+        document.getElementById('f5').innerHTML='passward not matching'
+        return false;
+    }
+    var a5=document.getElementById('cp').value;
+    if(a5==""){
+        document.getElementById('f5').innerHTML='Please fill this input';
+        return false;
+    }
+    
+}
